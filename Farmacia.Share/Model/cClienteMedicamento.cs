be955974cacbdf350978e66770e3d18dfcc9168a
Kendalgs <kengonz180@gmail.com>
2024-11-25 17:25:50 -0600

    Inicio
    Primero

---
+++ Farmacia.Share/Model/cClienteMedicamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmacia.Share.Model
+{
+    public class cClienteMedicamento
+    {
+        public short IdClienteMedicamento { get; set; }
+        public short IdMedicamento { get; set; }
+        public string Dosis { get; set; }
+        public cClienteMedicamento()
+        {
+
+            IdClienteMedicamento = 0;
+            IdMedicamento = 0;
+            Dosis = string.Empty;
+        }
+
+    }
+}
---
+++ Farmacia.Share/Model/cMedicamento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmacia.Share.Model
+{
+    public class cMedicamento
+    {
+
+        public short IdMedicamento { get; set; }
+        public string Descripcion { get; set; }
+        public string Presentacion { get; set; }
+        public string Marca { get; set; }
+        public string Estado { get; set; }
+
+        public cMedicamento()
+        {
+
+            IdMedicamento = 0;
+            Descripcion = string.Empty;
+            Presentacion = string.Empty;
+            Marca = string.Empty;
+            Estado = string.Empty;
+
+        }
+
+    }
+}
